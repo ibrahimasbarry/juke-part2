13861c7ee51e5549bf5821790fd6abb55f3e2e27
April Rueb <aprilrueb@gmail.com>
2017-10-09 10:51:12 -0400

    added keys

--- browser/react/Main.js
+++ browser/react/Main.js
@@ -16,27 +16,27 @@ export default class Main extends React.Component {
                     {
                       id: 1,
                       name: 'Romeo & Juliette',
-                      artists: [ 
-                        { name: 'Bill' } 
+                      artists: [
+                        { name: 'Bill' }
                       ],
                       genre: 'Funk',
                       audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-                    }, 
+                    },
                     {
                       id: 2,
                       name: 'White Rabbit',
                       artists: [
-                        { name: 'Bill' }, 
+                        { name: 'Bill' },
                         { name: 'Alice' }
                       ],
                       genre: 'Fantasy',
                       audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-                    }, 
+                    },
                     {
                       id: 3,
                       name: 'Lucy in the Sky with Diamonds',
-                      artists: [ 
-                        { name: 'Bob' } 
+                      artists: [
+                        { name: 'Bob' }
                       ],
                       genre: 'Space',
                       audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
@@ -51,8 +51,8 @@ export default class Main extends React.Component {
                     {
                       id: 4,
                       name: 'London Calling',
-                      artists: [ 
-                        { name: 'Bill' } 
+                      artists: [
+                        { name: 'Bill' }
                       ],
                       genre: 'Punk',
                       audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
@@ -69,15 +69,15 @@ export default class Main extends React.Component {
                 <div className="col-xs-10">
                 <h3>Albums</h3>
                 <div className="row">
-                {this.state.albums.map((ele)=> 
-                    (<div className="col-xs-4">
+                {this.state.albums.map((ele) =>
+                    (<div key={ele.id} className="col-xs-4">
                         <a className="thumbnail" href="#">
                             <img src="http://placeholdit.imgix.net/~text?txtsize=33&txt=ALBUMoneIMAGE&w=300&h=300" />
                             <div className="caption">
                                 <h5>
                                     <span>{ele.name}</span>
                                 </h5>
-                                <small>{ele.songs.length}</small>
+                                <small>{ele.songs.length + ' songs'}</small>
                             </div>
                         </a>
                     </div>)
